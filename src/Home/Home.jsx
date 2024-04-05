3b4c3e53fcd052d79aa7926c9da57a59ca8da262
valenpe <valentinaperez230701@gmail.com>
2024-04-05 16:18:03 -0300

    avance con el header, arreglar el espacio que me genera el scroll

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import "./Home.css";
 
 const Home = () => {
-  return <div>Hola</div>;
+  return (
+    <div className="containerHome">
+      <div className="textprincipal">
+        “nunca olvides <br /> por que empezaste”
+      </div>
+      <div className="textsecundario">
+        <p>
+          Una vez escuche, que la vida es como una hoja en blanco y que <br />{" "}
+          somos nosotros los encargados de trazar las líneas de nuestro futuro.{" "}
+          <br />
+          Te invito a apreciar mis primeros trazos.{" "}
+        </p>
+      </div>
+      <div className="waves">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 320">
+          <path
+            fill="#010f8d"
+            fill-opacity="1"
+            d="M0,64L48,80C96,96,192,128,288,138.7C384,149,480,139,576,160C672,181,768,235,864,224C960,213,1056,139,1152,96C1248,53,1344,43,1392,37.3L1440,32L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
+          ></path>
+        </svg>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
